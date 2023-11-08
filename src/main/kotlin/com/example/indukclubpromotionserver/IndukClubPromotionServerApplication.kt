65b6b85e27a15d53707b1e614ff0bbdcd29dc538
HanSangWook <qazws78941@gmail.com>
2023-11-08 19:05:50 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/example/indukclubpromotionserver/IndukClubPromotionServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.indukclubpromotionserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IndukClubPromotionServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<IndukClubPromotionServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/indukclubpromotionserver/IndukClubPromotionServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.indukclubpromotionserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IndukClubPromotionServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
