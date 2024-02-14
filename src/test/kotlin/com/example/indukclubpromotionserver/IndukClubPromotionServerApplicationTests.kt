26f7b1d76a0b514ed8529485e872be955a212652
HanSangWook <qazws78941@gmail.com>
2024-02-14 10:18:41 +0900

    테스트 주석 처리

--- src/test/kotlin/com/example/indukclubpromotionserver/IndukClubPromotionServerApplicationTests.kt
+++ src/test/kotlin/com/example/indukclubpromotionserver/IndukClubPromotionServerApplicationTests.kt
@@ -1,9 +1,8 @@
 package com.example.indukclubpromotionserver
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+//@SpringBootTest
 class IndukClubPromotionServerApplicationTests {
 
     @Test
