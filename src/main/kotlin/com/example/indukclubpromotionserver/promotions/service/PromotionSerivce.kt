4e8b832aa30047eda74a39cc8f37328f7f60b54b
HanSangWook <qazws78941@gmail.com>
2024-02-21 19:16:35 +0900

    프로젝트 완성

--- src/main/kotlin/com/example/indukclubpromotionserver/promotions/service/PromotionSerivce.kt
+++ src/main/kotlin/com/example/indukclubpromotionserver/promotions/service/PromotionSerivce.kt
@@ -4,7 +4,6 @@ import com.example.indukclubpromotionserver.promotions.dto.PromotionRequestDto
 import com.example.indukclubpromotionserver.promotions.dto.PromotionResponseDto
 import com.example.indukclubpromotionserver.promotions.entity.Promotion
 import com.example.indukclubpromotionserver.promotions.repository.PromotionRepository
-import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -18,88 +17,18 @@ class PromotionSerivce {
     @Autowired
     private lateinit var repository : PromotionRepository
 
-    @PostConstruct
-    fun loadData() {
-        repository.saveAll(
-            listOf(
-                Promotion(
-                    id = 1,
-                    userId = 1,
-                    club = "A&I",
-                    title = "A&I 신규 인원 모집",
-                    content = "2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.",
-                    createAt = LocalDateTime.now(),
-                    closeAt = LocalDateTime.now(),
-                    begin = LocalDateTime.now(),
-                    end = LocalDateTime.now(),
-                    contentOfActivity = "UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.",
-                    requiredPeople = 10,
-                    url = "https://docs.google.com/forms/d/e/1FAIpQLSeE64sjD3ymrPw3wEdjKT11oEUqOz2UW9TuoevhAiRB73CAbg/viewform?usp=sf_link",
-                    images = listOf<String>(
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3l9C58Dj31BhnXcMBMqLcL7T0foOUcsuEEg&usqp=CAU"
-                    )
-                ),
-                Promotion(
-                    id = 2,
-                    userId = 2,
-                    club = "A&I",
-                    title = "A&I 신규 인원 모집",
-                    content = "2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.",
-                    createAt = LocalDateTime.now(),
-                    closeAt = LocalDateTime.now(),
-                    begin = LocalDateTime.now(),
-                    end = LocalDateTime.now(),
-                    contentOfActivity = "UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.",
-                    requiredPeople = 10,
-                    url = "https://docs.google.com/forms/d/e/1FAIpQLSeE64sjD3ymrPw3wEdjKT11oEUqOz2UW9TuoevhAiRB73CAbg/viewform?usp=sf_link",
-                    images = listOf<String>(
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3l9C58Dj31BhnXcMBMqLcL7T0foOUcsuEEg&usqp=CAU"
-                    )
-                ),
-                Promotion(
-                    id = 3,
-                    userId = 3,
-                    club = "A&I",
-                    title = "A&I 신규 인원 모집",
-                    content = "2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.",
-                    createAt = LocalDateTime.now(),
-                    closeAt = LocalDateTime.now(),
-                    begin = LocalDateTime.now(),
-                    end = LocalDateTime.now(),
-                    contentOfActivity = "UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.",
-                    requiredPeople = 10,
-                    url = "https://docs.google.com/forms/d/e/1FAIpQLSeE64sjD3ymrPw3wEdjKT11oEUqOz2UW9TuoevhAiRB73CAbg/viewform?usp=sf_link",
-                    images = listOf<String>(
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3l9C58Dj31BhnXcMBMqLcL7T0foOUcsuEEg&usqp=CAU"
-                    )
-                ),
-                Promotion(
-                    id = 4,
-                    userId = 4,
-                    club = "A&I",
-                    title = "A&I 신규 인원 모집",
-                    content = "2022학년도에 진행한 스터디그룹 활동으로 처음 모여 모바일 앱 개발 공부 및 협업 프로젝트를 진행했으며, 플레이스토어와 앱스토어에 정식 출시까비 했습니다. 그리고 현재는 새로운 프로젝트를 준비하고 있습니다.",
-                    createAt = LocalDateTime.now(),
-                    closeAt = LocalDateTime.now(),
-                    begin = LocalDateTime.now(),
-                    end = LocalDateTime.now(),
-                    contentOfActivity = "UI/UX 디자인  프로젝트 화면 (UI)을 디자인하고, 이를 위한 미디어 파일 제작합니다.",
-                    requiredPeople = 10,
-                    url = "https://docs.google.com/forms/d/e/1FAIpQLSeE64sjD3ymrPw3wEdjKT11oEUqOz2UW9TuoevhAiRB73CAbg/viewform?usp=sf_link",
-                    images = listOf<String>(
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3l9C58Dj31BhnXcMBMqLcL7T0foOUcsuEEg&usqp=CAU"
-                    )
-                ),
-            )
 
-        )
-    }
-
+    /**
+     * 홍보글 가져오기
+     */
     fun getPromotions() : List<PromotionResponseDto> {
         val promotions = repository.findAll()
         return promotions.map { it.toResponse() }
     }
 
+    /**
+     * 홍보글 작성하기
+     */
     fun postPromotion(promotionRequestDto: PromotionRequestDto, userId : Long) : PromotionResponseDto {
 
         val promotion = Promotion(
